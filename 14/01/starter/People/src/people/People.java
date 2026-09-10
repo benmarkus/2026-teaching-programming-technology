536211ba85187f4159356aba997c861faef3267e
benmarkus <markusbence0@gmail.com>
2026-09-10 21:18:57 +0200

    add 14/01/starter

---
+++ 14/01/starter/People/src/people/People.java
@@ -0,0 +1,9 @@
+package people;
+
+public class People {
+
+    public static void main(String[] args) {
+        // TODO: create a Person, change their age, then print the new age
+    }
+
+}
---
+++ 14/01/starter/Queue/src/queue/TestQueue.java
@@ -0,0 +1,8 @@
+package queue;
+
+public class TestQueue {
+
+    public static void main(String[] args) {
+        // TODO: implement a Queue class (push, pop, size) and test it here
+    }
+}
